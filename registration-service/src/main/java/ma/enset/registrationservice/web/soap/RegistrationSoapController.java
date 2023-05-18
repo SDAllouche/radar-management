42fc77f2bdde335a6970e92e005ae9d242d43092
SALAHEDDINE ALLOUCHE <sdalami123@gmail.com>
2023-05-18 19:04:29 +0100

    "updating"

--- registration-service/src/main/java/ma/enset/registrationservice/web/soap/RegistrationSoapController.java
+++ registration-service/src/main/java/ma/enset/registrationservice/web/soap/RegistrationSoapController.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Component
-@WebService
+@WebService(serviceName = "CarService")
 @AllArgsConstructor
 public class RegistrationSoapController {
 
